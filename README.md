1769ab55473e53015b80e112751b7800c28a59a9
Urska Komatar <urskak2000@gmail.com>
2020-05-06 18:14:49 +0200

    dodali smo izpis prastevil

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Vislice
 Projekt vislice na vajah iz predmeta Uvod v Programiranje
+
+## Znani slovenski matematiki:
+- Jurij Vega
+- Ivan Vidav
+- Josip Plemelj
---
+++ test.py
@@ -0,0 +1,18 @@
+def je_prastevilo(n):
+    if n <= 2:
+        return n == 2
+    elif n % 2 == 0:
+        return False
+    else:
+        d = 3
+        while d ** 2 <= n:
+            if n % d == 0:
+                return False
+            d += 2
+        return True
+
+for i in range(200):
+    if je_prastevilo(i):
+        print(i)
+        
+
